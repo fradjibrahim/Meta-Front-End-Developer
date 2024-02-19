10bc64e08bd3a1c1ebaa2612320df856b83d8450
Ibrahim Fradj <Fradj.ibrahim@gmail.com>
2024-02-19 17:35:03 -0500

    exercice with multiple component using props

--- course5/src/App.js
+++ course5/src/App.js
@@ -1,9 +1,13 @@
 import Heading from "./Heading";
-
+import Card from "./Card";
+import "./App.css"
 function App() {
     return (
         <div className="App">
             <Heading firstName="Bob"/>
+            <Card h2="First card's h2" h3="First card's h3" />
+            <Card h2="Second card's h2" h3="Second card's h3" />
+            <Card h2="Third card's h2" h3="Third card's h3"/>
         </div>
     );
 };
