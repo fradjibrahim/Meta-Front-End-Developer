994e26e552ce06b4bb1b9ddf858cdc1976adc39d
Ibrahim Fradj <Fradj.ibrahim@gmail.com>
2024-02-20 18:36:47 -0500

    Managing state in React

--- course5/src/App.js
+++ course5/src/App.js
@@ -1,6 +1,10 @@
 import Heading from "./Heading";
 import Card from "./Card";
 import "./App.css"
+import Fruits from "./Fruits";
+import FruitsCounter from "./FruitCounter";
+import { useState } from "react";
+
 function App() {
     function handleClick() {
         let randomNum = Math.floor(Math.random() * 3) + 1;
@@ -8,6 +12,12 @@ function App() {
         let userInput = prompt('type a number');
         alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
     }
+
+    const [fruits] = useState([
+        {fruitName: 'apple', id: 1},
+        {fruitName: 'apple', id: 2},
+        {fruitName: 'plum', id: 3},
+    ]);
     return (
         <div className="App">
             <Heading firstName="Bob"/>
@@ -16,6 +26,9 @@ function App() {
             <Card h2="Third card's h2" h3="Third card's h3"/>
             <h1>Task: Add a button and handle a click event</h1>
             <button onClick={handleClick}>Guess the number between 1 and 3</button>
+
+            <Fruits fruits={fruits} />
+            <FruitsCounter fruits={fruits}/>
         </div>
     );
 };
